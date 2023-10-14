c6f6b1c3ffcf0d69c4b46d92dbad9a28b50e6f3d
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-10-14 13:52:12 +0530

    n = int(input()) arr = list(map(int, input().split())) c = 0 for i in range(1,n-1):     if arr[i-1]%2 == 0 and arr[i+1]%2 != 0:         c += 1     elif arr[i-1]%2 != 0 and arr[i+1]%2 == 0:         c += 1 print(c)

---
+++ Count_numbers_between_even_and_odd_numbers_or_vice_versa.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+c = 0
+for i in range(1,n-1):
+    if arr[i-1]%2 == 0 and arr[i+1]%2 != 0:
+        c += 1
+    elif arr[i-1]%2 != 0 and arr[i+1]%2 == 0:
+        c += 1
+print(c)
