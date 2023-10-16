ace87737e8863fa0cd249cfb4ce31f6dcd8589f5
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-10-16 19:37:43 +0530

    t = int(input()) for i in range(t):     s = input()     if '1' in s or '2' in s or '3' in s or '4' in s or '5' in s or '6' in s or '7' in s or '8' in s or '9' in s or '0' in s:         print("Yes")     else:         print("No")

---
+++ string_contains_digits_or_not.py
@@ -0,0 +1,7 @@
+t = int(input())
+for i in range(t):
+    s = input()
+    if '1' in s or '2' in s or '3' in s or '4' in s or '5' in s or '6' in s or '7' in s or '8' in s or '9' in s or '0' in s:
+        print("Yes")
+    else:
+        print("No")
