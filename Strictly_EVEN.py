41b7cd6adb38f25308a748bbc89113740419caae
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-10-18 20:23:53 +0530

    n = int(input()) arr = list(map(int, input().split())) flag = False for i in range(n):     if arr[i]%2 == 0 or arr[i] == 0:         if i%2 == 0:             flag = True         else:             flag = False             break print(flag == True)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+flag = False
+for i in range(n):
+    if arr[i]%2 == 0 or arr[i] == 0:
+        if i%2 == 0:
+            flag = True
+        else:
+            flag = False
+            break
+print(flag == True)
+    
